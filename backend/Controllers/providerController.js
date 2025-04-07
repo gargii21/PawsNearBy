f68e7b1a845d10794b6315de9e4782ec00442dde
gargii21 <segargirahane@gmail.com>
2025-04-07 09:31:56 +0530

    integrated provider form and other things

--- backend/Controllers/providerController.js
+++ backend/Controllers/providerController.js
@@ -1,16 +1,22 @@
-import { providerModel } from '../Config/db1.js';
+import { providerModel, userModel } from '../Config/db1.js';
 import { getCoordinates } from './userController.js';
 
 export const addProvider = async (req, res) => {
     //console.log("🔹 Route Hit: Data Received =>", req.body);  // Debugging log
-    
-    const { daycare_name, owner_name, phone, address, email, password, service, review, fees } = req.body;
 
+    if (!req.user || !req.user.userId) {
+        console.log(req.user)
+        return res.status(401).json({ message: 'Unauthorized. Please log in or sign up first.' });
+    }
+    
+    const { daycare_name, owner_name, phone, address, email, password, service, description, fees, experience } = req.body;
+    const userId= req.user.userId
     const cordinates= await getCoordinates(address);
-    //console.log(cordinates.latitude);
-    //console.log(cordinates.longitude);
+    console.log(cordinates.latitude);
+    console.log(cordinates.longitude);
     try {
         const result = await providerModel.create({
+            id:userId,
             daycare_name,
             owner_name,
             phone,
@@ -20,13 +26,18 @@ export const addProvider = async (req, res) => {
             service,
             prLatitude:cordinates.latitude,
             prLongitude:cordinates.longitude,
-            review,
-            fees
+            description,
+            fees,
+            experience
         });
+        await userModel.update(
+            { isProvider: true },
+            { where: { id: req.user.userId } }
+        );
 
         res.json({ success: true, data: result });
     } catch (error) {
-        console.log("Error adding provider:", error);
+        console.log("Error adding provider:", error);   
         res.status(500).json({ error: error.message });
     }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,8 +4,9 @@ import authMiddleware from '../middleware/authMiddleware.js';
 import {basic_signup, deleteUser, regUser, updateUser} from '../Controllers/userController.js';
 import search from '../Controllers/searchController.js';
 import { addProvider } from '../Controllers/providerController.js';
-import { sendRequest, acceptRequest, rejectRequest, getRequestStatus } from '../Controllers/requestController.js';
+//import { sendRequest, acceptRequest, rejectRequest, getRequestStatus } from '../Controllers/requestController.js';
 import  addPet  from '../Controllers/petController.js';
+import createRequest from '../Controllers/requestController.js';
 const router = Router();
 
 router.post("/login",authMiddleware,login);
@@ -16,10 +17,11 @@ router.post("/signup",authMiddleware,regUser);
 router.post("/updateInfo",authMiddleware,updateUser);
 
 router.post("/regProvider",authMiddleware,addProvider);
-router.post("/sendRequest",authMiddleware,sendRequest);
-router.post("/acceptRequest",authMiddleware,acceptRequest);
-router.post("/rejectRequest",authMiddleware,rejectRequest);
-router.get("/getRequestStatus",authMiddleware,getRequestStatus);
+// router.post("/sendRequest",authMiddleware,sendRequest);
+// router.post("/acceptRequest",authMiddleware,acceptRequest);
+// router.post("/rejectRequest",authMiddleware,rejectRequest);
+// router.get("/getRequestStatus",authMiddleware,getRequestStatus);
+router.post("/createRequest",authMiddleware,createRequest);
 router.delete("/delete",authMiddleware,deleteUser);
 router.post("/addPet",authMiddleware,addPet);
 
