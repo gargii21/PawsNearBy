544868dec925d1a65701b7cc3dee882b4db08ae6
Priyal Pandey <priyal.pandey@gmail.com>
2025-04-20 17:48:29 +0530

    test gh pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'your-repo-name' with your GitHub repo name
 export default defineConfig({
+  base: '/PawsNearBy/',
   plugins: [react()],
 })
